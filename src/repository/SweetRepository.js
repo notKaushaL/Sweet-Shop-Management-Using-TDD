08920432eab8274015e6d3361802ec25530be58b
notKaushaL <22airad052@ldce.ac.in>
2025-07-16 11:01:39 +0530

    Implement Sweet Deletion using UniqueID

--- src/repository/SweetRepository.js
+++ src/repository/SweetRepository.js
@@ -4,6 +4,9 @@ class SweetRepository {
     this.sweets = [];
   }
 
+
+  // Adds a sweet to the repository if ID is unique
+
   addSweet(sweet) {
     const exists = this.sweets.some(s => s.id === sweet.id);
     if (exists) {
@@ -12,9 +15,26 @@ class SweetRepository {
     this.sweets.push(sweet);
   }
 
+  
+  // Returns all sweets in the repository
+
   getAllSweets() {
     return this.sweets;
   }
+
+
+  // Deletes a sweet with the given ID, throws error if not found
+
+deleteSweetById(id) {
+  const index = this.sweets.findIndex(s => s.id === id);
+
+  if (index === -1) {
+    throw new Error(`Sweet with ID ${id} not found`);
+  }
+
+  this.sweets.splice(index, 1);
+}
+
 }
 
 module.exports = SweetRepository;
