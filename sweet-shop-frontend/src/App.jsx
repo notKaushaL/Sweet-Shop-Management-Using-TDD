9eebe11e1773c27708ddb80a0368328b162223d6
notKaushaL <22airad052@ldce.ac.in>
2025-07-17 15:01:12 +0530

    To check the configuration of tailwind with React

--- sweet-shop-frontend/src/App.jsx
+++ sweet-shop-frontend/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
       <div>
-        <p className="text-2xl"> Sweet Store Frontend </p>
+        <p className="text-2xl font-bold text-center"> Sweet Store Frontend </p>
       </div>
       
     </>
